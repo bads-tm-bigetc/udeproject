483af7b88c6fb5183b3e342a5653c42dce8b4d77
arc <arc@527297ee-e91a-0410-b169-9d91edc4108a>
2003-06-09 19:23:58 +0000

    some more passive focus stuff.

--- main/ude/uwm/widgets.h
+++ main/ude/uwm/widgets.h
@@ -2,7 +2,7 @@
 #define UDE_WIDGETS_H
 
 void DrawBevel(Drawable win,int x1,int y1,int x2,int y2,int width,GC NW,GC SE);
-void DrawFrameBevel(UltimateContext *uc,char Active);
-void DrawTitle(UltimateContext *uc,char Active);
+void DrawFrameBevel(UltimateContext *uc);
+void DrawTitle(UltimateContext *uc);
 
 #endif
