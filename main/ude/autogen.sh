cd9041ac13729476ac3d891ce52f220ca0140c28
arc <arc@527297ee-e91a-0410-b169-9d91edc4108a>
2003-12-30 18:19:31 +0000

    repaired sed-scripts for simpler versions of sed

--- main/ude/autogen.sh
+++ main/ude/autogen.sh
@@ -60,17 +60,14 @@ echo "============================== cleaning up possibly improper checkins"
 mv Makefile.am Makefile.am.origcvs
 echo "*** Makefile.am saved to Makefile.am.origcvs"
 cat Makefile.am.origcvs | sed '
-v
 /\\$/N
 /SUBDIRS[[:space:]]*=/ {
 s/\([=]\|[[:space:]]\)intl\([[:space:]]\|$\)/\1/g
 s/\([=]\|[[:space:]]\)m4\([[:space:]]\|$\)/\1/g
-s/\([^\n \t]\)[\t ]\{2,\}/\1 /g
 }
 /EXTRA_DIST[[:space:]]*=/ {
 s/\([=]\|[[:space:]]\)mkinstalldirs\([[:space:]]\|$\)/\1/g
 s/\([=]\|[[:space:]]\)config\.rpath\([[:space:]]\|$\)/\1/g
-s/\([^\n \t]\)[\t ]\{2,\}/\1 /g
 }
 /ACLOCAL_AMFLAGS[[:space:]]*=/ {
 d
@@ -79,14 +76,12 @@ d
 mv configure.in configure.in.origcvs
 echo "*** configure.in saved to configure.in.origcvs"
 cat configure.in.origcvs | sed '
-v
 /\\$/N
 /AC_OUTPUT[[:space:]]*(/ {
 s/\([=]\|[[:space:]]\)intl\/Makefile\([[:space:]]\|)\)/\1/g
 s/\([=]\|[[:space:]]\)po\/Makefile\.in\([[:space:]]\|)\)/\1/g
 s/\([=]\|[[:space:]]\)po\/Makefile\([[:space:]]\|)\)/\1/g
 s/\([=]\|[[:space:]]\)m4\/Makefile\([[:space:]]\|)\)/\1/g
-s/\([^\n \t]\)[\t ]\{2,\}/\1 /g
 }
 ' > configure.in
 
